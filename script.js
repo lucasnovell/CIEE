13bdf3767987d0747235461474cfc5aa61bd2821
Lucas Novelli <novellilucas15@gmail.com>
2022-09-08 23:06:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function comprar(){
-  document.getElementById("compra").classList.toggle("esconder")
-  document.getElementById("container").classList.toggle("esconder")
+  document.getElementById("compra").classList.remove("esconder")
+  document.getElementById("container").classList.add("esconder")
 }
